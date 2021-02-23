bb83efb2e22336315c40139b0cf719b898581208
radux18 <64654536+radux18@users.noreply.github.com>
2021-02-23 12:38:03 +0200

    Add files via upload

---
+++ Project_Just4Gamers/app/src/main/java/com/example/project_just4gamers/SpashActivity.java
@@ -0,0 +1,14 @@
+package com.example.project_just4gamers;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SpashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spash);
+    }
+}
